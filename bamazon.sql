d5e41d83c25735c2786a89aedab6e4c87cd35a71
Tashona Smith <tashona.smith@gmail.com>
2020-05-14 17:13:38 -0500

    Create bamazon database

---
+++ bamazon.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products(
+    item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(100) NOT NULL,
+    price INTEGER(2) NOT NULL,
+    stock_quanity INTEGER(11) NOT NULL,
+    PRIMARY KEY (item_id)
+);
